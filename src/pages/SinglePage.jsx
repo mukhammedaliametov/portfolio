67b530bb8c7ce0a40ef4a039ca036f7a81cbb121
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2024-03-12 00:50:25 +0500

    edit single page :)

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -10,8 +10,8 @@ const SinglePage = () => {
   return (
     <div className="flex justify-center w-full pt-[110px] min-h-screen bg-black/90">
       <div className="max-w-[1000px] mx-[30px] xl:max-w-[1200px] w-full">
-        <h1 className="text-4xl text-[#66FCF1] font-lspartan">
-          <Link to='/portfolio'>Portfolio</Link> / <span className="text-[2rem]">{selectedProject.name}</span>
+        <h1 className="text-2xl md:text-4xl text-[#66FCF1] font-lspartan">
+          <Link to='/portfolio'>Portfolio</Link> / <span className="text-[1.4rem] md:text-[2rem]">{selectedProject.name}</span>
         </h1>
         <span className="py-[2px] block rounded-lg bg-[#66FCF1] w-[5%]"></span>
         <div className="flex items-center justify-evenly gap-8 my-8">
@@ -19,9 +19,15 @@ const SinglePage = () => {
             src={selectedProject.img}
             alt={selectedProject.name}
             width={650}
-            className="rounded-xl"
+            className="rounded-xl hidden lg:block"
           />
           <div className="text-white font-montserrat flex flex-col gap-2">
+          <img
+            src={selectedProject.img}
+            alt={selectedProject.name}
+            width={650}
+            className="lg:hidden rounded-xl"
+          />
             <h1 className="text-2xl mb-2">{selectedProject.name}</h1>
             <p>
               <b>Technologies:</b> {selectedProject.technologies}
